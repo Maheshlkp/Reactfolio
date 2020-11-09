49112b55528878a636544cb51901b8f9a759c509
Mahesh <Mahesh.lkp1234@gmail.com>
2020-11-09 13:47:31 -0600

    added images

--- src/components/title-message/title-message.component.jsx
+++ src/components/title-message/title-message.component.jsx
@@ -39,7 +39,7 @@ const TitleMessage = () => (
           Hi, I am
           <br />
           <span>
-            <strong>Anand Kumar Jha</strong>
+            <strong>Mahesh Lakkapally</strong>
           </span>
         </div>
         <div className="sub">
